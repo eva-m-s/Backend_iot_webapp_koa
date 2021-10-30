a26b69cbf8f5722a652cbc78cd8fc344dfdb19c8
eva-m-s <88543636+eva-m-s@users.noreply.github.com>
2021-10-30 11:41:26 +0200

    plants.js edited

--- server/plants.js
+++ server/plants.js
@@ -1,5 +1,5 @@
 'use strict';
-const plants=[
+/*const plants=[
 	{
 	id: 1,
 	type: 'Kaktusowate' ,
@@ -55,5 +55,60 @@ const plants=[
 			  light: [2,3,2] 
 		  },
 	  ]}
-  ] 
+  ] */
+const plants=[
+{
+  id: 1,
+  type: 'Inny' ,
+  species:[
+	  {
+		  name: 'Inny',
+		  nr: 11,
+		  temp: [1,1,1,2] ,
+		  hum: [2,2,2,2] ,
+		  soil: [2,1,1,0],
+		  light: [2,2,3,2] 
+	  },
+  ]},
+{
+id: 2,
+type: 'Fiołkowate' ,
+species:[
+{
+	nr: 22,
+	name: 'Fiołek alpejski',
+	temp: 1 ,
+	hum: 2 ,
+	soil: 1 ,
+	light: 2 
+},
+]},
+{
+id: 3,
+type: 'Pieprzowate' ,
+species:[
+	{
+		nr: 33,
+		name: 'Peperomia',
+		temp: 2 ,
+		hum: 2 ,
+		soil: 0 ,
+		light: 2 
+	},
+]},
+{
+id: 4,
+type: 'Wrzosowate' ,
+species:[
+	{
+		nr: 44,
+		name: 'Azalia',
+		temp: 1 ,
+		hum: 2 ,
+		soil: 1 ,
+		light: 3
+	},
+]}
+]
+
 module.exports = plants;
